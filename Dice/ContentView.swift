1ff232b524e6415f7ecdac15e1af6e21879166f7
Jacek Kosinski <kashiash@gmail.com>
2024-05-07 13:53:15 +0200

    .

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,17 +8,163 @@
 import SwiftUI
 
 struct ContentView: View {
+    let  size: CGFloat = 180
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        LazyVGrid(columns: [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+
+        ]) {
+            Dice1()
+                .frame(width: size, height: size)
+            Dice2()
+                .frame(width: size, height: size)
+            Dice3()
+                .frame(width: size, height: size)
+            Dice4()
+                .frame(width: size, height: size)
+            Dice5()
+                .frame(width: size, height: size)
+            Dice6()
+                .frame(width: size, height: size)
+        }.padding()
+    }
+}
+
+struct Dice1: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            Dot()
         }
-        .padding()
+    }
+}
+struct Dice2: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            VStack {
+                HStack {
+                    Spacer()
+                    Dot()
+                }
+                Spacer()
+                HStack {
+                    Dot()
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+struct Dice3: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            VStack{
+
+                HStack {
+                    Dot()
+                    Spacer()
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Dot()
+                }
+            }
+           Dot()
+        }
+    }
+}
+struct Dice4: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            VStack{
+
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+                Spacer()
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+            }
+        }
+    }
+}
+struct Dice5: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            VStack{
+
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+                Spacer()
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+
+                }
+
+            }
+            Dot()
+        }
+    }
+}
+struct Dice6: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .stroke(lineWidth: 2.0)
+            VStack {
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+
+                Spacer()
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+                Spacer()
+                HStack {
+                    Dot()
+                    Spacer()
+                    Dot()
+                }
+            }
+        }
+    }
+}
+
+struct Dot: View {
+    var body: some View {
+        Circle()
+            .frame(width: 20,height: 20)
+            .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
