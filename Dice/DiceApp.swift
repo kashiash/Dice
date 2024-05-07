3c9b75a592f4fdc5e918375a2ed52360aa2abc26
Jacek Kosinski <kashiash@gmail.com>
2024-05-07 09:36:28 +0200

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Jacek Kosinski U on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
